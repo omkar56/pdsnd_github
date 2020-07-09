aa181b516ac34df78df9bd938a525afdbf643a55
Omkar Masurekar <omkarmasurekar18@gmail.com>
2020-07-09 18:10:58 +0530

    added bikeshare

--- bikeshare.py
+++ bikeshare.py
@@ -16,8 +16,16 @@ weekdays = ('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday',
 
 
 def choice(prompt, choices=('y', 'n')):
-    """Return a valid input from the user given an array of possible answers.
-    """
+    """Return a valid input from the
+user given an array of possible
+answers.
++
++ Args:
++ (str) prompt - prompt with
+input request
++ (tup) choices - tuple with
+elements of possible answers
+"""
 
     while True:
         choice = input(prompt).lower().strip()
